98f1fd393699a50a9b4d0015fae16716cf60e199
Richard Jonáš <jonas.richard11@gmail.com>
2023-10-02 22:02:28 +0200

    startin project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data"
+import { useState } from "react"
 
-function App() {
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    const amount = parseInt(count)
+    console.log(typeof(amount))
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>
+      <h1>Lorem ipsum generátor</h1>
+      <form onSubmit={submitForm}>
+        <label htmlFor="paragraphs">Odstavce:</label>
+        <input type="number" id="paragraphs" onChange={ (e) => {setCount(e.target.value)} }/><br />
+        <input type="submit" value="Vygenerovat"/>
+      </form>
+    </section>
+  )
 }
 
-export default App;
+
+export default App
